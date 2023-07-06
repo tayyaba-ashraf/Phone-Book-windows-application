24bfe569c962fb125d6a89873d360d4a60882afb
tayyaba <138785438+tayyaba-ashraf@users.noreply.github.com>
2023-07-06 18:26:59 +0500

    Add files via upload
    these will show you design and backend implementation for the user pages who want to use this application as client on his/her machine

---
+++ Edit_User_Data_Form.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace PhoneBookWindowsApplication_VisualProject
+{
+    partial class Edit_User_Data_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.Close_button = new System.Windows.Forms.Button();
+            this.Minimize_Button = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.ID_text = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Edit_button = new System.Windows.Forms.Button();
+            this.password_text = new System.Windows.Forms.TextBox();
+            this.UName_text = new System.Windows.Forms.TextBox();
+            this.Email_text = new System.Windows.Forms.TextBox();
+            this.FName_text = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider2 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider3 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider4 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider5 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Purple;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(1, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(592, 417);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(187, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 20);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "EDIT DATA USER FORM ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.SlateBlue;
+            this.panel3.Controls.Add(this.Close_button);
+            this.panel3.Controls.Add(this.Minimize_Button);
+            this.panel3.Location = new System.Drawing.Point(379, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(213, 47);
+            this.panel3.TabIndex = 2;
+            // 
+            // Close_button
+            // 
+            this.Close_button.BackColor = System.Drawing.Color.Gray;
+            this.Close_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Close_button.Location = new System.Drawing.Point(116, 3);
+            this.Close_button.Name = "Close_button";
+            this.Close_button.Size = new System.Drawing.Size(94, 41);
+            this.Close_button.TabIndex = 1;
+            this.Close_button.Text = "X";
+            this.Close_button.UseVisualStyleBackColor = false;
+            this.Close_button.Click += new System.EventHandler(this.Close_button_Click);
+            // 
+            // Minimize_Button
+            // 
+            this.Minimize_Button.BackColor = System.Drawing.Color.DimGray;
+            this.Minimize_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Minimize_Button.Location = new System.Drawing.Point(3, 2);
+            this.Minimize_Button.Name = "Minimize_Button";
+            this.Minimize_Button.Size = new System.Drawing.Size(107, 41);
+            this.Minimize_Button.TabIndex = 0;
+            this.Minimize_Button.Text = "--";
+            this.Minimize_Button.UseVisualStyleBackColor = false;
+            this.Minimize_Button.Click += new System.EventHandler(this.Minimize_Button_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel2.Controls.Add(this.ID_text);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.Edit_button);
+            this.panel2.Controls.Add(this.password_text);
+            this.panel2.Controls.Add(this.UName_text);
+            this.panel2.Controls.Add(this.Email_text);
+            this.panel2.Controls.Add(this.FName_text);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Location = new System.Drawing.Point(3, 52);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(586, 362);
+            this.panel2.TabIndex = 0;
+            // 
+            // ID_text
+            // 
+            this.ID_text.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.ID_text.Location = new System.Drawing.Point(206, 15);
+            this.ID_text.Name = "ID_text";
+            this.ID_text.Size = new System.Drawing.Size(172, 20);
+            this.ID_text.TabIndex = 0;
+            this.ID_text.MouseLeave += new System.EventHandler(this.ID_text_MouseLeave);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Lavender;
+            this.label2.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(52, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 20);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "USER ID";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Edit_button
+            // 
+            this.Edit_button.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Edit_button.Location = new System.Drawing.Point(140, 243);
+            this.Edit_button.Name = "Edit_button";
+            this.Edit_button.Size = new System.Drawing.Size(267, 61);
+            this.Edit_button.TabIndex = 5;
+            this.Edit_button.Text = "EDIT";
+            this.Edit_button.UseVisualStyleBackColor = true;
+            this.Edit_button.Click += new System.EventHandler(this.Edit_button_Click);
+            // 
+            // password_text
+            // 
+            this.password_text.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.password_text.Location = new System.Drawing.Point(206, 195);
+            this.password_text.Name = "password_text";
+            this.password_text.Size = new System.Drawing.Size(172, 20);
+            this.password_text.TabIndex = 4;
+            // 
+            // UName_text
+            // 
+            this.UName_text.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.UName_text.Location = new System.Drawing.Point(206, 152);
+            this.UName_text.Name = "UName_text";
+            this.UName_text.Size = new System.Drawing.Size(172, 20);
+            this.UName_text.TabIndex = 3;
+            // 
+            // Email_text
+            // 
+            this.Email_text.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.Email_text.Location = new System.Drawing.Point(206, 100);
+            this.Email_text.Name = "Email_text";
+            this.Email_text.Size = new System.Drawing.Size(172, 20);
+            this.Email_text.TabIndex = 2;
+            // 
+            // FName_text
+            // 
+            this.FName_text.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.FName_text.Location = new System.Drawing.Point(206, 56);
+            this.FName_text.Name = "FName_text";
+            this.FName_text.Size = new System.Drawing.Size(172, 20);
+            this.FName_text.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Lavender;
+            this.label3.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(52, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "FIRST NAME";
+            // 
+            // label8
+            // 
+            this.label8.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Lavender;
+            this.label8.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(52, 100);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(45, 20);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "EMAIL";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Lavender;
+            this.label6.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(52, 192);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "PASSWORD";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Lavender;
+            this.label5.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(52, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "USERNAME";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // errorProvider2
+            // 
+            this.errorProvider2.ContainerControl = this;
+            // 
+            // errorProvider3
+            // 
+            this.errorProvider3.ContainerControl = this;
+            // 
+            // errorProvider4
+            // 
+            this.errorProvider4.ContainerControl = this;
+            // 
+            // errorProvider5
+            // 
+            this.errorProvider5.ContainerControl = this;
+            // 
+            // Edit_User_Data_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 420);
+            this.Controls.Add(this.panel1);
+            this.Name = "Edit_User_Data_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Edit_User_Data_Form";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider5)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button Close_button;
+        private System.Windows.Forms.Button Minimize_Button;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox FName_text;
+        private System.Windows.Forms.TextBox Email_text;
+        private System.Windows.Forms.TextBox UName_text;
+        private System.Windows.Forms.Button Edit_button;
+        private System.Windows.Forms.TextBox password_text;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ID_text;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.ErrorProvider errorProvider2;
+        private System.Windows.Forms.ErrorProvider errorProvider3;
+        private System.Windows.Forms.ErrorProvider errorProvider4;
+        private System.Windows.Forms.ErrorProvider errorProvider5;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Edit_User_Data_Form.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;  // namespace of refrence system.configuration for connection with database
+using System.Data.SqlClient;
+
+namespace PhoneBookWindowsApplication_VisualProject
+{
+    public partial class Edit_User_Data_Form : Form
+    {
+        public Edit_User_Data_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Close_button_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Minimize_Button_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void Edit_button_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(ID_text.Text) == true)
+            {
+                ID_text.Focus();
+                errorProvider1.SetError(this.ID_text, "Please Fill the ID !!");
+
+            }
+            else if (string.IsNullOrEmpty(FName_text.Text) == true)
+            {
+                FName_text.Focus();
+                errorProvider2.SetError(this.FName_text, "Please Fill the First Name !!");
+
+            }
+            else if (string.IsNullOrEmpty(Email_text.Text) == true)
+            {
+                Email_text.Focus();
+                errorProvider3.SetError(this.Email_text, "Please Fill the EMAIL !!");
+
+            }
+            else if (string.IsNullOrEmpty(UName_text.Text) == true)
+            {
+                UName_text.Focus();
+                errorProvider4.SetError(this.UName_text, "Please Fill the USERNAME !!");
+
+            }
+            else if (string.IsNullOrEmpty(password_text.Text) == true)
+            {
+                password_text.Focus();
+                errorProvider5.SetError(this.password_text, "Please Fill the PASSWORD!!");
+
+            }
+         
+
+            else
+            {
+
+                string cs = ConfigurationManager.ConnectionStrings["dbcs"].ConnectionString;
+                SqlConnection con = new SqlConnection(cs);
+
+
+                string query = "select * from signuptable where USERID=@userId";
+                //@userId Parameter name is used 
+                //this will get value from ID_text textbox
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@userId", ID_text.Text);
+
+                //we will newly open and close connection for select query execution
+
+                con.Open();
+                //ExecuteReader commnad is used for execution slect query
+                //    SqlDataReader will read row from database table
+
+                SqlDataReader Rd = cmd.ExecuteReader();
+                if (Rd.HasRows == true)
+                {
+                    con.Close();
+
+
+                    string query2 = " update  signuptable set Fname=@fname,Email=@email,username=@username,pass=@pass where USERID=@userId";
+                    SqlCommand cmd2 = new SqlCommand(query2, con);
+
+
+                    // if enterd data is not entering on already exeisting id
+                    //then all below data of row will insert
+
+
+                    // these values are passed coulmn wise
+
+                    cmd2.Parameters.AddWithValue("@userId", ID_text.Text);
+                    cmd2.Parameters.AddWithValue("@fname", FName_text.Text);
+                    cmd2.Parameters.AddWithValue("@email", Email_text.Text);
+                    cmd2.Parameters.AddWithValue("@username", UName_text.Text);
+                    cmd2.Parameters.AddWithValue("@pass", password_text.Text);
+
+
+
+
+                    //now for execution of above written query
+                    //connection with database should open newly
+                    // and ExecuteNonQuery command will use for execution of insert query and update
+
+                    con.Open();
+
+                    int a = cmd2.ExecuteNonQuery();
+                    if (a > 0)
+                    {
+
+
+
+                        MessageBox.Show(" EDITED SUCCESSFULLY !!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show(" EDITED FAILED !! ", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+
+                    //close connection opend for execution of insert query
+
+                    con.Close();
+                }
+
+                /* private void label2_Click(object sender, EventArgs e)
+                 {
+
+                 }*/
+
+            }
+
+        }
+
+        private void ID_text_MouseLeave(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
